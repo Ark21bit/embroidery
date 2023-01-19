32482bf6078fa1ef0733d4c803a603c966c824bf
Ark21bit <webleif@vk.com>
2023-01-20 01:35:40 +0300

    категории

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,11 +1,26 @@
-const swiper = new Swiper('.swiper', {
+const swiperMain = new Swiper('.swiperMain', {
     // Optional parameters    
     loop: true,
     spaceBetween:32,
+    
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
     },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }, 
+    
+  });
+
+const swiperSecond = new Swiper('.swiperSecond', {
+    // Optional parameters 
+    spaceBetween:32,
+    slidesPerView:5,
+    // If we need pagination   
   
     // Navigation arrows
     navigation: {
